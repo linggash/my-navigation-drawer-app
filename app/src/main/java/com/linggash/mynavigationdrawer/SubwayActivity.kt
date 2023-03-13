c3a20d7d439f1609e3c202e9da0d80f42a8b0987
linggash <sempanalingga@gmail.com>
2023-03-13 20:54:49 +0700

    navigation drawer with navigation component practice

---
+++ app/src/main/java/com/linggash/mynavigationdrawer/SubwayActivity.kt
@@ -0,0 +1,12 @@
+package com.linggash.mynavigationdrawer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.linggash.mynavigationdrawer.R
+
+class SubwayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subway)
+    }
+}
